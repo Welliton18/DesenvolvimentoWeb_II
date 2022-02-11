68c04b7e0e593d12af8fd2ed1ead1b82b97525ec
Welliton Luiz Becker <chaank18@gmail.com>
2022-02-10 21:36:53 -0300

    Alterado

--- libs/a.php
+++ libs/a.php
@@ -9,6 +9,8 @@ class a {
 
     private $url;
     private $texto;
+    private $target = "_blanck";
+    private $class;
 
     public function setUrl($sUrl){
         $this->url = $sUrl;
@@ -27,7 +29,23 @@ class a {
     }
 
     public function getLink(){
-        return "<a href={$this->getUrl()}>{$this->getTexto()} </a>";
+        return "<a href={$this->getUrl()} target={$this->getTarget()} class={$this->getClass()}>{$this->getTexto()} </a>";
+    }
+    
+    public function setTarget($sTarget)    {
+        $this->target = $sTarget;
+    }
+
+    public function getTarget(){
+        return $this->target;
+    }
+    
+    public function setClass($class){
+        $this->class = $class;
+    }
+
+    public function getClass(){
+        return $this->class;
     }
 
 }
@@ -35,6 +53,9 @@ class a {
 $oLink = new a();
 $oLink->setUrl('https://www.unidavi.edu.br');
 $oLink->setTexto('Unidavi');
+$oLink->setClass('botao');
+$oLink->setTarget('_parent');
+
 echo $oLink->getLink();
 echo '<br>';
 $oLink2 = new a();
