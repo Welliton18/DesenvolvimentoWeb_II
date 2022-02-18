93fe50b81b011ef34a0c9e94cac65c44561a3290
Welliton Luiz Becker <chaank18@gmail.com>
2022-02-17 21:26:59 -0300

    alterado

--- index.php
+++ index.php
@@ -2,11 +2,6 @@
 
 require ('autoload.php');
 
-$oHead = new head([]);
-$oHead->addItem(new meta('charset="UTF-8"'));
-$oHead->addItem(new meta('http-equiv="X-UA-Compatible" content="IE=edge"'));
-$oHead->addItem(new meta('name="viewport" content="width=device-width, initial-scale=1.0"'));
-$oHead->addItem(new title('Teste'));
 
 $aColunas = [
     new td('campo', 'Coluna 1'),
@@ -21,4 +16,14 @@ $aLinhas = [
 ];
 $oBody = new body([new table('tabela', $aLinhas)]);
 
-echo new html([$oHead, $oBody], 'pt-br');
+echo new html([criaHead(), $oBody], 'pt-br');
+
+
+function criaHead(){
+    $oHead = new head([]);
+    $oHead->addItem(new meta('charset="UTF-8"'));
+    $oHead->addItem(new meta('http-equiv="X-UA-Compatible" content="IE=edge"'));
+    $oHead->addItem(new meta('name="viewport" content="width=device-width, initial-scale=1.0"'));
+    $oHead->addItem(new title('Teste'));
+    return $oHead;
+}
